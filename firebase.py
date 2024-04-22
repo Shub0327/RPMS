8247e683c292cdd23faaa3da193c18fc4a4eb5bf
shub <shubhammankar2703@gmail.com>
2024-04-23 00:09:09 +0530

    update

--- firebase.py
+++ firebase.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import csv
 
 # Initialize Firebase app
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -30,3 +31,42 @@ def retrieve_data():
 
 #call the function
 retrieve_data()
+
+def convert_to_csv(data):
+    keys = ['is_leakage_detected', 'flow_rate_1', 'flow_rate_2', 'tds_value', 'temperature']
+    filename = 'H:\\Major Project\\major\\sensor_data.csv'
+
+    with open(filename, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=keys)
+        writer.writeheader()
+        writer.writerow(data)
+
+    print("Data converted to CSV successfully.")
+
+# Example usage:
+data = {
+    'is_leakage_detected': True,
+    'flow_rate_1': 0.5,
+    'flow_rate_2': 0.6,
+    'tds_value': 0.7,
+    'temperature': 0.8
+}
+
+convert_to_csv(data)
+
+def send_data_to_firebase(is_leakage_detected, flow_rate_1, flow_rate_2, tds_value, temperature):
+    data = {
+        'is_leakage_detected': is_leakage_detected,
+        'flow_rate_1': flow_rate_1,
+        'flow_rate_2': flow_rate_2,
+        'tds_value': tds_value,
+        'temperature': temperature
+    }
+    # Create a new reference in the database
+    new_ref = db.reference('leakage_data')
+
+    # Set the data in the new reference
+    new_ref.set(data)
+
+#call the function
+send_data_to_firebase(True, 0.5, 0.6, 0.7, 0.8)
